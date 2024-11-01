53f5498afcc31d8fd678f1b73a280bf6df3d9060
ejieflorida128 <ejieflorida128@gmail.com>
2024-11-01 14:20:32 +0800

    [ejie]
    changes

--- User/dashboard.php
+++ User/dashboard.php
@@ -236,7 +236,7 @@
                 <div class="bg-light rounded-top p-4">
                     <div class="row">
                         <div class="col-12 col-sm-6 text-center text-sm-start">
-                            &copy; <a href="../index.php">Library Management System</a>, All Right Reserved. 
+                            &copy; <a href="../index.php">Library Management System by: Group LMS</a>, All Right Reserved. 
                         </div>
                         <div class="col-12 col-sm-6 text-center text-sm-end">
                           
